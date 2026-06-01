71efc0822308bb95b59b4de37696335735b4b0c3
Jose Santaella <jasantaella15@gmail.com>
2026-06-01 19:32:07 +0200

    feat: add success colors

--- apps/web/src/app.vue
+++ apps/web/src/app.vue
@@ -8,6 +8,8 @@ import {
   TabsList,
   TabsTrigger,
   ChartFilters,
+  Separator,
+  Badge,
   type ChartFilterModel,
 } from "ui";
 import {
@@ -92,22 +94,36 @@ const tickerOptions = computed(() =>
   <main class="h-screen p-2 md:p-8 flex justify-center items-center flex-col">
     <DashboardCard>
       <ChartFilters :markets="MARKETS_OPTIONS" :tickerOptions="tickerOptions" v-model="filters" />
-      <div class="flex gap-4 justify-center md:justify-between" v-if="
+      <div class="flex gap-4 justify-center md:justify-start" v-if="
         filters.selectedTicker && filters.market == ListTickersMarketEnum.Fx
       ">
+        <img
+          :src="`https://wise.com/public-resources/assets/flags/rectangle/${selectedTickerData?.base_currency_symbol?.toLowerCase()}.png`"
+          :alt="selectedTickerData?.base_currency_name" />
+        <img
+          :src="`https://wise.com/public-resources/assets/flags/rectangle/${selectedTickerData?.currency_symbol?.toLowerCase()}.png`"
+          :alt="selectedTickerData?.currency_name" />
+        <h2 class="text-xl font-bold">
+          {{ selectedTickerData?.base_currency_symbol }} -
+          {{ selectedTickerData?.currency_symbol }}
+        </h2>
+      </div>
+      <div class="flex flex-col justify-center items-center gap-4 md:justify-between md:items-start md:flex-row">
         <div class="flex gap-4">
-          <img
-            :src="`https://wise.com/public-resources/assets/flags/rectangle/${selectedTickerData?.base_currency_symbol?.toLowerCase()}.png`"
-            :alt="selectedTickerData?.base_currency_name" />
-          <img
-            :src="`https://wise.com/public-resources/assets/flags/rectangle/${selectedTickerData?.currency_symbol?.toLowerCase()}.png`"
-            :alt="selectedTickerData?.currency_name" />
-          <h2 class="text-xl font-bold">
-            {{ selectedTickerData?.base_currency_symbol }} -
-            {{ selectedTickerData?.currency_symbol }}
-          </h2>
+          <div>
+            <p class="text-xs uppercase">{{ $t("common.exchange") }}</p>
+            <span class="text-xl font-bold uppercase">{{ filters.market }}</span>
+          </div>
+          <div>
+            <p class="text-xs uppercase">{{ $t("dashboard.label.currentPrice") }}</p>
+            <span class="text-xl font-bold uppercase">0.01232</span>
+          </div>
+        </div>
+        <div>
+          <Badge class="px-4 py-2 text-md font-bold" variant="success"> 0.23123 (1.41%)</Badge>
         </div>
       </div>
+      <Separator class="my-4" />
       <Tabs v-model="timeRange" class="overflow-x-auto">
         <TabsList>
           <TabsTrigger :value="tab" v-for="tab in TABS" class="p-4">
